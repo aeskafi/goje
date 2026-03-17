bae39ddabe7956a55e3d05808571c603fc08d336
aeskafi <arham.eskafi@gmail.com>
2026-03-17 11:02:46 +0300

    chore: setup Capacitor for Android and iOS deployment

---
+++ android/app/src/main/java/com/aeskafi/goje/MainActivity.java
@@ -0,0 +1,5 @@
+package com.aeskafi.goje;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aeskafi.goje',
+  appName: 'GOJE TRAINING',
+  webDir: 'out'
+};
+
+export default config;
